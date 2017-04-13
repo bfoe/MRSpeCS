b54d862db8887677ee5199b0ea29c554e709699f
bfoe <bfoerster@gmail.com>
2017-04-13 19:17:40 -0300

    Delete PyInstaller_bulid.bat

--- Windows_supportfiles/PyInstaller_bulid.bat
+++
@@ -1,9 +0,0 @@
-C:\Python27\scripts\pyinstaller PyInstaller.spec
-rmdir /Q /S ..\Windows_binary\*.*
-mkdir ..\Windows_binary\*.*
-copy /Y dist\MRSpeCS.exe ..\Windows_binary\*.*
-copy /Y README_Windows.txt ..\Windows_binary\*.*
-copy /Y ..\MRSpeCS.py ..\Windows_binary\*.*
-rmdir /Q /S build
-rmdir /Q /S dist
-pause
